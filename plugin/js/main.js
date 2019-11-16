6249e9ae91617addb00cdd23dd65b6cd96a3adb8
Orxan-dev <54859397+Orxan-dev@users.noreply.github.com>
2019-11-16 17:33:54 +0400

    Add files via upload

---
+++ plugin/js/main.js
@@ -0,0 +1,34 @@
+$('.sliderss').slick({
+  dots: true,
+  infinite: true,
+  autoplayspeed: 500,
+  fade: true,
+  speed: 700,
+  autoplay: true,
+  cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
